7e13235bdf39be09c903cf9fba7c510c7356a844
zhil <starsfm@gmail.com>
2017-07-28 11:47:39 +0300

    removed code

--- src/AppBundle/Command/TestCommand.php
+++ src/AppBundle/Command/TestCommand.php
@@ -31,68 +31,6 @@ class TestCommand extends ContainerAwareCommand
             $client = new Client($this->getApplication()->getKernel());
             $output->writeln(sprintf('Fetching <comment>%s</comment>', $url));
             $client->request('GET', $url);
-            $i = $this->countEntities();
-            if (($i - $currentEntitiesCount) > 200) {
-                $output->writeln(sprintf('Loaded <error>%s</error> entities - too much', $i - $currentEntitiesCount));
-            } else {
-                $output->writeln(sprintf('Loaded <info>%s</info> entities.', $i - $currentEntitiesCount));
-            }
-            $currentEntitiesCount = $i;
-        }
-
-        $entitiesLimit = [
-            Vendor::class => 10
-        ];
-
-        foreach ($entitiesLimit as $class => $limit) {
-            $this->unloadEntities($class,$limit);
-        }
-
-        $fixturesContext = FixtureGenerationContext::create()
-            ->setMaximumRecursion(1)
-            ->setSkipNotInitializedCollections(true);
-        $output->write('Generating dump ...');
-        $yaml = $this->getContainer()->get('trappar_alice_generator.fixture_generator')->generateYaml(
-            $this->getEntities($output),
-            $fixturesContext
-        );
-        file_put_contents($input->getArgument('file'), PatchAliceGenerator::patchIssue12($yaml));
-        $output->writeln('Done!');
-    }
-
-    private function countEntities()
-    {
-        $count = 0;
-        foreach ($this->getContainer()->get('doctrine.orm.default_entity_manager')->getUnitOfWork()->getIdentityMap() as $entityType) {
-            $count += count($entityType);
-        }
-        return $count;
-    }
-
-    private function getEntities(OutputInterface $output = null)
-    {
-        $entities = [];
-        foreach ($this->getContainer()->get('doctrine.orm.default_entity_manager')->getUnitOfWork()->getIdentityMap() as $class=>$entityType) {
-            if($output) {
-                $output->writeln(sprintf('%s - %s',$class,count($entityType)));
-            }
-            $entities = array_merge($entities, $entityType);
-        }
-        return $entities;
-    }
-
-    private function unloadEntities($class, $allowedCount)
-    {
-        $em = $this->getContainer()->get('doctrine.orm.default_entity_manager');
-        $identityMap = $em->getUnitOfWork()->getIdentityMap();
-        $entities = $identityMap[$class];
-        if(count($entities) > $allowedCount) {
-            foreach (array_slice($entities,$allowedCount) as $entity) {
-                $em->detach($entity);
-            }
-            return $allowedCount;
-        } else {
-            return count($entities);
         }
     }
 }
